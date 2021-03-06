e7b0401b4c3d89fe2928bb235b4e34486ee36f0e
unknown <okinawanspud@gmail.com>
2021-03-06 15:39:42 +0900

    az15.py complete@

--- az15.py
+++ az15.py
@@ -13,33 +13,36 @@ def printPrompt():
             "[176] [188] [199] [200] [210] [222] [1]   [10]  [20]  [47]  [59]  [63]  [75]  [88]  [99]  [107] [120] [133] [155] [162]\n"
             "Runtime Complexity: Logarithmic,O(logn)\n"
             "Memory Complexity: Logarithmic,O(logn)\n")
+
+# key<Mid LSorted key>Lmin RSorted key<Rmax Dir
+#    1       1      1         x       x      L
+#    1       1      0         x       x      R
+#    1       0      x         x       x      L
+#    0       x      x         1       1      R
+#    0       x      x         1       0      L
+#    0       x      x         0       x      R
+
+
 def binary_search_rotated(arr, key):
-    arrLen=len(arr)
-    cIdx=arrLen//2
-    count=2
-    while(arr[cIdx] != key):
-        step=arrLen//(2*count)
-        print("cIdx="+str(cIdx)+" step="+str(step))
-        if step==0:
-            step=1
-        leftSorted=False
-        rightSorted=False
-        if arr[cIdx-step]<arr[cIdx]:
-            leftSorted=True
-        if arr[cIdx]<arr[cIdx+step]:
-            rightSorted=True
-        if leftSorted:
-            leftMin=arr[cIdx-(2*step)]
-            if key < arr[cIdx] and key >= leftMin:
-                cIdx-=step
-        else:
-            rightMax=arr[cIdx+(2*step)]
-            if key > arr[cIdx] and key <= rightMax:
-                cIdx+=step
+    s=0#start
+    e=len(arr)-1
+    m=((e-s)//2)+s
+    while(arr[m] != key):
+        #print("s,m,e = "+str(s)+","+str(m)+","+str(e))
+        if s==e:
+            return -1
+        if key < arr[m]:
+            if arr[s] < arr[m] and key < arr[s]:
+                s=m+1
             else:
-                cIdx-=step
-                print("HOW DID I GET HERE?!?")
-    return cIdx
+                e=m-1
+        else:
+            if arr[m] < arr[e] and key > arr[e]:
+                e=m-1
+            else:
+                s=m+1
+        m=((e-s)//2)+s
+    return m
 
 
 def test():
@@ -57,22 +60,26 @@ def test():
         counter+=1
     tar1=[176,188,199,200,210,222,1,10,20,47,59,63,75,88,99,107,120,133,155,162]
     counter=0
-    for iter in tar1:
-        testVector.append(testCase(tar1,iter,counter))
-        counter+=1
-    tar2=[47,59,63,75,88,99,107,120,133,155,162,176,188,199,200,210,222,1,10,20]
-    counter=0
-    for iter in tar2:
-        testVector.append(testCase(tar2,iter,counter))
-        counter+=1
+
+    additional=True
+    if additional:
+        for iter in tar1:
+            testVector.append(testCase(tar1,iter,counter))
+            counter+=1
+        tar2=[47,59,63,75,88,99,107,120,133,155,162,176,188,199,200,210,222,1,10,20]
+        counter=0
+        for iter in tar2:
+            testVector.append(testCase(tar2,iter,counter))
+            counter+=1
 
     counter=0
     for test in testVector:
-        if counter>1:
-            break
+        # if counter>1:
+            # return
         result=binary_search_rotated(test.arr,test.key)
         if test.expectation!=result:
             print("FAIL!!! Array "+str(test.arr)+ " with key "+str(test.key)+" expects "+str(test.expectation)+" but produced "+str(result)+" !")
         else:
             print("Pass.")
+        counter+=1
 test()
