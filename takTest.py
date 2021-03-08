ebb43959ac194b2f40660afa303068e2256ab564
DJTakushi <okinawanspud@gmail.com>
2021-03-08 18:34:47 +0900

    Update takTest.py

--- takTest.py
+++ takTest.py
@@ -1,3 +1,8 @@
+import unittest
+class tData:
+    def print(self):
+        for key,value in vars(self).items():
+            print("  "+key+ " = "+str(value))
 class testCase:
     def __init__(self,data,expectation):
         self.data=data
@@ -5,6 +10,15 @@ class testCase:
     def print(self):
         self.data.print()
         print("  expectation = "+str(self.expectation))
+class tunittest(unittest.TestCase):
+    def test_executeTests(self):
+        for test in self.makeTestVector():
+            result=functionWrapper(test.data)
+            if result != test.expectation:
+                print("FAIL!!! For test case: ")
+                test.print()
+                print("  received "+str(result))
+            self.assertEqual(test.expectation, result)
 def test(testFunctionW,testVector):
     for test in testVector:
         result=testFunctionW(test.data)
@@ -12,5 +26,6 @@ def test(testFunctionW,testVector):
             print("FAIL!!! For test case: ")
             test.print()
             print(" received "+str(result))
-            return
+            return False
     print(" pass.")
+    return True
