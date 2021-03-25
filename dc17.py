6866492ef4195c56d8d54109550428906a2157be
DJTakushi <okinawanspud@gmail.com>
2021-03-25 18:32:20 +0900

    Update dc17.py

--- dc17.py
+++ dc17.py
@@ -41,21 +41,30 @@ def printProblem():
             "The name of a directory or sub-directory will not contain a period.")
 
 def maxFilePathLen(fs):
+    recordString = ""
     path=list()
     eList=fs.split("\\")
     while len(eList) != 0:
         if len(path)==0:
-            path.append(eList.pop(0)+"/")
+            path.append(eList.pop(0))
         level=0
-        while eList[0] == "n" or eList[0] == "t":
+        while eList[0] == "n" or eList[0] == "t":#get level from markers
             eList.pop(0)
             level+=1
-        while len(path)>level:
+        while len(path)>level: #trim current path until appropriate
             path.pop(-1)
-        name=eList[0]
+        name=eList.pop(0)
         name=name[1:] #get rid of starting t
+        path.append(name)
         if "." in name:
-
+            fileString=""
+            for i in range(len(path)):
+                fileString+=path[i]
+                if i < len(path)-1:
+                    fileString+="/"
+            if len(fileString)>len(recordString):
+                recordString=fileString
+    return len(recordString)
 
 
 
