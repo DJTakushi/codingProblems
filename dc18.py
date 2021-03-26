aa424dba69fd888c7e62839461ef4274d81189fd
DJTakushi <okinawanspud@gmail.com>
2021-03-26 18:44:58 +0900

    Add files via upload

---
+++ dc18.py
@@ -0,0 +1,15 @@
+def printProblem():
+    print("Daily Coding Problem: Problem #18 [Hard]\n"
+            "This problem was asked by Google.\n"
+            "Given an array of integers and a number k, where 1 <= k <= length\n"
+            "of the array, compute the maximum values of each subarray of\n"
+            "length k.\n"
+            "For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we\n"
+            "should get: [10, 7, 8, 8], since:\n"
+            "  10 = max(10, 5, 2)\n"
+            "  7 = max(5, 2, 7)\n"
+            "  8 = max(2, 7, 8)\n"
+            "  8 = max(7, 8, 7)\n"
+            "Do this in O(n) time and O(k) space. You can modify the input\n"
+            "array in-place and you do not need to store the results. You can\n"
+            "simply print them out as you compute them.")
