53efbb8ebe763021e7e146c4e625dac24c649492
unknown <okinawanspud@gmail.com>
2021-03-31 22:13:08 +0900

    dc23 solved with recursion

--- dc23.py
+++ dc23.py
@@ -30,11 +30,55 @@ def getC(point,dir):
         return (point[0],point[1]-1)
 
 def solve(b,start,end,pPath=None):
-    up=(start[0]-1,start)
-
+    #print("start = ("+str(start[0])+","+str(start[1])+"), end = ("+str(end[0])+","+str(end[1])+")")
+    if not pPath:
+        pPath = set()
+    if start == end:
+        #print(" got it!  Set list = "+str(len(pPath)))
+        return len(pPath)#hit target - return path length
+    else:
+        if start in pPath: #overlapping path is stupid.  ABORT!
+            return -1
+        if start[0] >= len(b) or start[0] < 0:#y out of range
+            return -1
+        if start[1] >= len(b[start[0]]) or start[1] < 0:#x out of range
+            return -1
+        if b[start[0]][start[1]]=='t': #we're in a wall.  ABORT!
+            return -1
+        pPath.add(start)
+        rl=list()
+        rl.append(solve(b,getC(start,"n"),end,pPath.copy()))
+        rl.append(solve(b,getC(start,"s"),end,pPath.copy()))
+        rl.append(solve(b,getC(start,"e"),end,pPath.copy()))
+        rl.append(solve(b,getC(start,"w"),end,pPath.copy()))
+        if len(rl):
+            min=max(rl)
+            for i in rl:
+                if i < min and i != -1:
+                    min = i
+            return min
+        else:
+            return -1
 
 import unittest
 class myTest(unittest.TestCase):
     def testThis(self):
         board=[['f', 'f', 'f', 'f'],['t', 't', 'f', 't'],['f', 'f', 'f', 'f'],['f', 'f', 'f', 'f']]
         self.assertEqual(7,solve(board,(3,0),(0,0)))
+
+        #fail due to no path available
+        board=[['f', 'f', 'f', 'f'],['t', 't', 't', 't'],['f', 'f', 'f', 'f'],['f', 'f', 'f', 'f']]
+        self.assertEqual(-1,solve(board,(3,0),(0,0)))
+
+        # " [[f, f, f, f],\n" zig-zag all the way
+        # "  [t, t, t, f],\n"
+        # "  [f, f, f, f],\n"
+        # "  [f, t, t, t],\n"
+        # "  [f, f, f, f]]\n"
+        board=[['f', 'f', 'f', 'f'],['t', 't', 't', 'f'],['f', 'f', 'f', 'f'],['f', 't', 't', 't'],['f', 'f', 'f', 'f']]
+        self.assertEqual(13,solve(board,(4,3),(0,0)))
+        board=[['f', 'f', 'f', 'f'],['t', 't', 't', 'f'],['f', 'f', 'f', 'f'],['f', 't', 't', 't'],['f', 'f', 'f', 'f']]
+        self.assertEqual(10,solve(board,(4,0),(0,0)))
+
+if __name__=="__main__":
+    unittest.main()
