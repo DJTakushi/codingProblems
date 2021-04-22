b43ae5017deb821583363b238121e9d3db974770
unknown <okinawanspud@gmail.com>
2021-04-22 22:27:41 +0900

    dc47 problem .so add3d

---
+++ dc47/prob.cpp
@@ -0,0 +1,10 @@
+#include <string>
+std::string prob(void)
+{
+  std::string output;
+  output += "Daily Coding Problem: Problem #47 [Easy]\n";
+  output += "This problem was asked by Facebook.\n";
+  output += "Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.\n";
+  output += "For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.";
+  return output;
+}
