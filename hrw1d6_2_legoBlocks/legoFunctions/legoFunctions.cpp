847752218becb98125db5289d7c36b262ad6eef3
DJTakushi <okinawanspud@gmail.com>
2022-10-02 12:22:06 -0500

    update to pass by references for faster execution

--- hrw1d6_2_legoBlocks/legoFunctions/legoFunctions.cpp
+++ hrw1d6_2_legoBlocks/legoFunctions/legoFunctions.cpp
@@ -3,36 +3,37 @@
 #include <chrono>
 #include <list>
 #include <math.h>
-vector<vector<int>> getVector(int l, int max){
+void getVector(int l, int max, vector<vector<int>>* out){
 		/** l = length of row
 				max = max block length **/
-		vector<vector<int>> out;
+		// vector<vector<int>> out;
 		if(l>=0)
 		{
 				if(l==0){
 						vector<int> tmp;
-						out.push_back(tmp);
+						out->push_back(tmp);
 				}else if(l==1){
 						vector<int> tmp;
 						tmp.push_back(1);
-						out.push_back(tmp);
+						out->push_back(tmp);
 					} else if(l > 1)
 					{
 						for(int i = 1; i <= max; i++){
 							int newl = l-i;
 							if(newl>=0)
 							{
-								vector<vector<int>> recursive = getVector(newl,max);
+								vector<vector<int>> recursive;
+								getVector(newl,max, &recursive);
 								for(auto it = recursive.begin();it!=recursive.end();it++)
 								{
 									it->insert(it->begin(),i);
-									out.push_back(*it);
+									out->push_back(*it);
 								}
 							}
 						}
 					}
 				}
-    return out;
+
 }
 set<int> getCrackIdx(vector<int> i){
 	/** return set of indexes in a row where cracks occur for the block arrangement**/
@@ -45,33 +46,30 @@ set<int> getCrackIdx(vector<int> i){
 	}
 	return out;
 }
-set<int> setAnd(set<int> a,set<int>b){
+void setAnd(set<int>* a, set<int>* b, set<int>* out){
 	/*** returns the AND of two sets ***/
-	set<int> out;
-	for(auto it = a.begin(); it!=a.end(); it++)
+	for(auto it = a->begin(); it!=a->end(); it++)
 	{
-		if(b.find(*it)!=b.end()) out.insert(*it);
+		if(b->find(*it)!=b->end()) out->insert(*it);
 	}
-	return out;
 }
-int countBadWalls(int n, list<set<int>>* options, set<int> crackIdxs)
-{
-	//recursive count the number of bad iterations
+int countBadWalls(int n, list<set<int>>* options, set<int>* crackIdxs)
+{	//recursive count the number of bad iterations
 	int output = 0;
 	if(n==0){
-		if(crackIdxs.size()>0)output = 1;//cracks exist - count this as bad
+		if(crackIdxs->size()>0)output = 1;//cracks exist - count this as bad
 		else output = 0;//cracks do not exist - bad
 	}
 	else
 	{
-		if(crackIdxs.size()>0)
+		if(crackIdxs->size()>0)
 		{
 			for(auto it= options->begin(); it != options->end(); it++)
 			{
 				set<int>crackIdxs_t;
 				// crackIdxs_t = crackIdxs;
-				crackIdxs_t = setAnd(crackIdxs, *it);//continue any shared cracks, forgive any that are mended
-				output+=countBadWalls(n-1,options,crackIdxs_t);//increment by recursive call
+				setAnd(crackIdxs, &(*it), &crackIdxs_t);//continue any shared cracks, forgive any that are mended
+				output+=countBadWalls(n-1,options,&crackIdxs_t);//increment by recursive call
 			}
 		}
 	}
@@ -85,7 +83,8 @@ int legoBlocks(int n, int m){
 	auto startT = start;
 	int out = 0;
 
-	vector<vector<int>> uro = getVector(m,4);  //unrestricted row options
+	vector<vector<int>> uro;  //unrestricted row options
+ 	getVector(m,4, &uro);
 	auto getVectorTimeDiff = chrono::steady_clock::now()-startT;
 
 	startT = chrono::steady_clock::now();
@@ -103,11 +102,11 @@ int legoBlocks(int n, int m){
 	for(int i = 0; i < m; i++)
 		crackIdxsBlank.insert(i);
 	if(n >0){
-			out = pow(uroList.size(), n)- countBadWalls(n,&uroList,crackIdxsBlank);
+			out = pow(uroList.size(), n)- countBadWalls(n,&uroList,&crackIdxsBlank);
 	}
 	auto buildWallsTime = chrono::steady_clock::now()-startT;
 	auto totalDiff = chrono::steady_clock::now()-start;
-
+	chrono::duration<double> totalDiff_s = chrono::duration_cast<chrono::duration<double>>(totalDiff);
 	long td_0 = getVectorTimeDiff.count();
 	long td_1 = get_uroSetTime.count();
 	long td_2 = buildWallsTime.count();
@@ -115,14 +114,11 @@ int legoBlocks(int n, int m){
 	float tdp_0 = 100*float(td_0)/td_t;
 	float tdp_1 = 100*float(td_1)/td_t;
 	float tdp_2 = 100*float(td_2)/td_t;
-	// cout <<td_t<<endl;
-	// cout << tdp_0 <<endl;
-	// cout << tdp_1 <<endl;
-	// cout << tdp_2 <<endl;
 
 	cout <<"    completed getVector("<<m<<","<<4<<") in time diff "<<td_0<<"("<<tdp_0<<"%)"<<endl;
 	cout <<"    completed getting uroSet in time diff "<<td_1<<"("<<tdp_1<<"%)"<<endl;
 	cout <<"    completed buildWalls in time diff "<<td_2<<"("<<tdp_2<<"%)"<<endl;
+	cout <<"    completed in "<<totalDiff_s.count()<<" seconds"<<endl;
 	return out;
 }
 #ifdef PRINT_FUNCTIONS
