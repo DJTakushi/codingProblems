91cd42c2f0eae2cdc00b511988df762376e1497d
DJTakushi <okinawanspud@gmail.com>
2022-10-01 22:57:17 -0500

    testing expanded from tests on HackerRank

--- hrw1d6_2_legoBlocks/tests/test.cpp
+++ hrw1d6_2_legoBlocks/tests/test.cpp
@@ -39,6 +39,54 @@ int testSharedLibrary(void){
   assert(exp==o);
   cout <<"pass"<<endl;
 
+  n=4;
+  m=5;
+  exp=35714;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
+  n=4;
+  m=6;
+  exp=447902;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
+  n=4;
+  m=7;
+  exp=5562914;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
+  n=5;
+  m=4;
+  exp=29791;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
+  n=6;
+  m=4;
+  exp=250047;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
+  n=7;
+  m=4;
+  exp=2048383;
+  cout << "testing legoBlocks("<<n<<","<<m<<") == "<<exp<<"...";
+  o = legoBlocks(n,m);
+  assert(exp==o);
+  cout <<"pass"<<endl;
+
   return 0;
 }
 
@@ -46,11 +94,13 @@ int testInterface(void){
   //https://pubs.opengroup.org/onlinepubs/9699919799/functions/popen.html
   cout <<"testInterface()..."<<endl;
 
+
   string command ="./lb 1 \"2 3\"";
-  cout << "testing command: " <<command <<"...";
+  string exp = "9";
+  cout << "testing command: " <<command <<" returns "<<exp<<"...";
   vector<string> o = commandToStrings(command);
   assert(1 == o.size());
-  assert("9"==trim(o[0]));
+  assert(exp==trim(o[0]));
   cout <<"pass."<<endl;
 
   return 0;
