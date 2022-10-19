724489245c67743465e19b0dad3ff16b6ca20bc6
DJTakushi <okinawanspud@gmail.com>
2022-10-18 22:54:16 -0500

    DRY refactor and simplification

--- lc0005_longestPalindromicSubstring/longestPalindromicSubstring/longestPalindromicSubstring.cpp
+++ lc0005_longestPalindromicSubstring/longestPalindromicSubstring/longestPalindromicSubstring.cpp
@@ -1,72 +1,49 @@
 #include <longestPalindromicSubstring.h>
 #include <map>
 
-string Solution::longestPalindrome(string s) {
-    string o = s.substr(0,1);
-    int o_l = o.size();//o length
-    int s_l = s.size();//s length
+string Solution::getPalindromeAtRoot(string s, int i, size_t left, size_t right){
+  string o(1,s[i]); //output
+  size_t s_l = s.size();//s length
 
-//iterate along characters in string
+  // while left==right (it's still a palindrome)
+  while(s[left]==s[right]){
+    //  if can increase left and right indexes, do it
+    size_t leftNew = left-1;
+    size_t rightNew = right+1;
+    if((left>0 & right < s_l-1) //validate new indexes are in range
+          && (s[leftNew]==s[rightNew])){//validate new indexes are palindromic
+      left--;
+      right++;
+    }
+    //  if should not advance, update o with last palindrome string and break to return
+    else{
+      o=s.substr(left,right-left+1);
+      break;
+    }
+  }
+  return o;
+}
+
+string Solution::longestPalindrome(string s) {
+  string o = s.substr(0,1);
+
+  //iterate along characters in string
   for(size_t i = 0; i < s.size(); i++){
     string ot; //o temp
-
-    // set left and right indexes to this char
-    size_t left = i;
-    size_t right = i;
-
-    // while left==right (it's a palindrome)
-    while(s[left]==s[right]){//not necessary to check here if left==right
-      //  if can increase left and right indexes, do it
-
-      size_t leftNew = left-1;
-      size_t rightNew = right+1;
-      if((left>0 & right < s_l-1)
-            && (s[leftNew]==s[rightNew])){
-        left--;
-        right++;
-      }
-      //  if not, break
-      else break;
-    }
-    ot=s.substr(left,right-left+1);
-    cout << "i:"<<to_string(i)<<", ot="<<ot<<endl;
-    // record substring if a record
+    ot = this->getPalindromeAtRoot(s, i, i, i);//i is center of odd palindrome
     o = ot.size() > o.size() ? ot : o;
 
-    //TODO: rfactor this to make DRY code.
-    // Consider function prototype:
-    // string getPalindromeAtRoot(string s, int leftStart, int rightStart);
+    //can try even palindrome with symmetry between i and i+1
     if(i<s.size()-1){
-      // set left and right indexes to this char
-      left = i;
-      right = i+1;
-
-      // while left==right (it's a palindrome)
-      while(s[left]==s[right]){
-        //  if can increase left and right indexes, do it
-
-        size_t leftNew = left-1;
-        size_t rightNew = right+1;
-        if((left>0 & right < s_l-1)
-              && (s[leftNew]==s[rightNew])){
-          left--;
-          right++;
-        }
-        //  if not, break
-        else break;
-      }
-      if(s[left]==s[right]){//must check in case they were never equal
-        ot=s.substr(left,right-left+1);
-        cout << "i(2):"<<to_string(i)<<", ot="<<ot<<endl;
-        // record substring if a record
-        o = ot.size() > o.size() ? ot : o;
-      }
+      ot = this->getPalindromeAtRoot(s, i, i, i+1);//in case even palindrome
+      o = ot.size() > o.size() ? ot : o;
     }
-
   }
 
-// improvemnt! start in middle of s, and then iterate right/left.  Stop iterating if potential palindrome length is lower than record length
-    return o;
+  /* TODO: try starting in middle of s, and then iterate right/left.
+  Stop iterating if potential palindrome length is lower than record length.
+  Middle will contain the longest palindrom (E.G: `AAAAAAAAAAAAAAAAAAAAAA`)*/
+  return o;
 }
 
 vector<string> Solution::getDescription(){
