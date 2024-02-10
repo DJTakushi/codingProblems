e61f5a35b1e41c2af6791f48fa2964d776f88ca1
danny <dannytakushi@gmail.com>
2024-02-09 22:49:35 -0600

    gtest conversion; project name

--- lc_array_01/test.cpp
+++ lc_array_01/test.cpp
@@ -1,29 +1,22 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "remove_duplicates_from_sorted_array.h"
-#include <iostream>
-#include <string>
-#include <vector>
-void print_results(std::vector<int> v, int k){
-  std::string printout = "[";
-  for(auto i : v){
-    printout += (std::to_string(i) + ","); 
-  }
-  printout = printout.substr(0,printout.size()-1);
-  printout += std::string("] k = " + std::to_string(k));
-  std::cout <<printout <<std::endl;
 
-};
-int main(int argc, char * argv[]){
-  Solution s;
-  int result;
+int main(int argc, char* argv[]){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
-  std::vector<int> ex1 = {1,1,2};
-  result = s.removeDuplicates(ex1);
-  print_results(ex1,result);
-
-  std::vector<int> ex2 = {0,0,1,1,1,2,2,3,3,4};
-  result = s.removeDuplicates(ex2);
-  print_results(ex2,result);
-
-
-  std::cout <<"done"<<std::endl;
+class demoTest : public ::testing::Test, public Solution{};
+TEST_F(demoTest,example_1){
+  EXPECT_EQ(1,1);
+  std::vector<int> vec = {1,1,2};
+  EXPECT_EQ(2, removeDuplicates(vec));
+  EXPECT_THAT(vec,testing::ElementsAre(1,2));
+}
+TEST_F(demoTest,example_2){
+  EXPECT_EQ(1,1);
+  std::vector<int> vec = {0,0,1,1,1,2,2,3,3,4};
+  EXPECT_EQ(5, removeDuplicates(vec));
+  EXPECT_THAT(vec,testing::ElementsAre(0,1,2,3,4));
 }
