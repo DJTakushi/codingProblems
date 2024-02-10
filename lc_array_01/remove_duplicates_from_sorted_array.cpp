63d9a8b911ed81c58864382bdb6c248ea06463ad
danny <dannytakushi@gmail.com>
2024-02-09 23:53:24 -0600

    logic made more intuitive to me

--- lc_array_01/remove_duplicates_from_sorted_array.cpp
+++ lc_array_01/remove_duplicates_from_sorted_array.cpp
@@ -4,11 +4,12 @@ int Solution::removeDuplicates(std::vector<int>& nums) {
   std::vector<int>::iterator it = nums.begin();
   while(it < nums.end()){
     std::vector<int>::iterator jt = it;
-    while(jt < nums.end() && *jt == *it){
+    while((jt+1) < nums.end() && *(jt+1) == *it){
       jt++;
     }
-    if(*it == *(jt-1)){
-      nums.erase((it+1), jt);
+    if(it != jt){
+      //Removes the elements in the range [first, last).
+      nums.erase(it, jt);
     }
     it++;
   }
