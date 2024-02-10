b34275ba587f8bfe0297428ae81d51841b4b69b7
danny <dannytakushi@gmail.com>
2024-02-10 14:03:48 -0600

    solution implementation

--- lc_array_02/stock_trader.cpp
+++ lc_array_02/stock_trader.cpp
@@ -1,5 +1,13 @@
 #include "stock_trader.h"
 
 int Solution::maxProfit(std::vector<int>& prices){
-  return 0;
+  int profit = 0;
+  std::vector<int>::iterator it = prices.begin();
+  while(it != (prices.end()-1)){
+    if(*it < *(it+1)){
+      profit += *(it+1)-*it;
+    }
+    it++;
+  }
+  return profit;
 };
