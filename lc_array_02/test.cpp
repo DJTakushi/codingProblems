15e07d2cbf721cd0e1d0084b1cb2b79147cb355e
danny <dannytakushi@gmail.com>
2024-02-10 19:29:38 -0600

    benchmarking creation

--- lc_array_02/test.cpp
+++ lc_array_02/test.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <benchmark/benchmark.h>
 #include "stock_trader.h"
 
 int main(int argc, char* argv[]){
+  ::benchmark::RunSpecifiedBenchmarks();
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
@@ -22,3 +25,21 @@ TEST_F(demoTest,example_3){
   std::vector<int> vec = {7,6,4,3,1};
   EXPECT_EQ(0, maxProfit(vec));
 }
+
+static void case1(benchmark::State& state) {
+  Solution s;
+  for (auto _ : state){
+    std::vector<int> vec = {7,1,5,3,6,4};
+    s.maxProfit(vec);
+  }
+}
+BENCHMARK(case1);
+
+static void case2(benchmark::State& state) {
+  Solution s;
+  for (auto _ : state){
+    std::vector<int> vec = {7,1,5,3,6,4};
+    s.maxProfit(vec);
+  }
+}
+BENCHMARK(case2);
